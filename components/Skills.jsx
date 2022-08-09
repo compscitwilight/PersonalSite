3466ccf383768cff4ae77a11510c3aec3540f1d4
devrusty <rguillory246@gmail.com>
2022-08-09 00:37:17 -0500

    updated skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,21 +3,20 @@ export default function Skills() {
         <span className="skills page-section">
             <h2>Skills</h2>
             <p>
-                I mainly specialise in Javascript and Typescript development, and I am mostly familiar with Node.js, React.js, Next.js, and
-                Node.js.
+                I mainly specialise in Javascript and Typescript development, and I am mostly familiar with Node.js, React.js, and Next.js.
             </p>
             <div className="skills-list-container" id="skills-list-container">
                 <ul className="skills-list">
                     <li className="skill">Javascript</li>
                     <li className="skill">Typescript</li>
                     <li className="skill">Roblox Lua</li>
-                    <li className="skill">C#</li>
                     <li className="skill">HTML / CSS</li>
                     <li className="skill">SCSS</li>
                     <li className="skill">Express.js</li>
                     <li className="skill">React.js</li>
                     <li className="skill">Next.js</li>
                     <li className="skill">Node.js</li>
+                    <li className="skill">Discord.js</li>
                 </ul>
             </div>
         </span>
